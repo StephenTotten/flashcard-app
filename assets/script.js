d531238dc85dedbace3fe3af2efdb131690f556c
Crothos <s.g.totten@gmail.com>
2023-01-12 13:52:22 -0600

    WordsAPI returns random word and definition into console log

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ const number = ["0","1","2","3","4","5","6","7","8","9","10",];
 const random = Math.floor(Math.random() * number.length);
 console.log(random, number[random]);
 
-// WordsAPI Fetch Request, Returns Random Word and Console Logs it
+// WordsAPI Fetch Request, Returns Random Word/Definition and Console Logs it
 const options = {
 	method: 'GET',
 	headers: {
