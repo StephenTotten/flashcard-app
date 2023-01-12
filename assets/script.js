c997563ceafc19fbbb27b3818d8cd3b4555bc3a9
Crothos <s.g.totten@gmail.com>
2023-01-12 13:31:43 -0600

    WordsAPI returns random word

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,17 @@
 
 
-
+// Letters Arrays
 var alphaUpperArray = ["A", "B", "C", "D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var alphaLowerArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
+// Numbers Arrays
 const number = ["0","1","2","3","4","5","6","7","8","9","10",];
+
+// Getting random number from Array
 const random = Math.floor(Math.random() * number.length);
 console.log(random, number[random]);
 
+// WordsAPI Fetch Request, Returns Random Word and Console Logs it
 const options = {
 	method: 'GET',
 	headers: {
@@ -16,7 +20,7 @@ const options = {
 	}
 };
 
-fetch('https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf', options)
+fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
