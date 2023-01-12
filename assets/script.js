122acc64ef49b887e8a5b9c6c6e7e5d5756bb037
Holden Claus <holdenclaus0212@gmail.com>
2023-01-12 13:07:32 -0600

    Array for numbers in text

--- assets/script.js
+++ assets/script.js
@@ -4,5 +4,8 @@ function flashCard () {
 }
 
 const number = ["0","1","2","3","4","5","6","7","8","9","10",];
+const number2 = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten",];
 const random = Math.floor(Math.random() * number.length);
+const random2 = Math.floor(Math.random() * number2.length);
 console.log(random, number[random]);
+console.log(random, number[random2]);
