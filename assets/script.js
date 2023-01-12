46a7184a40c47a28b453e1bb5288933a434cbedb
Crothos <s.g.totten@gmail.com>
2023-01-12 11:53:57 -0600

    added uppercase and lowercase array to javascript file

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,6 @@ function flashCard () {
     // 
 
 }
+
+var alphaUpperArray = ["A", "B", "C", "D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var alphaLowerArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
