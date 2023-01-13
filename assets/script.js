66ccc224b640639e654f9f34bb39ee87f394b822
Holden Claus <holdenclaus0212@gmail.com>
2023-01-13 13:19:39 -0600

    Event Listeners added

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,35 @@
+var alphabetButton = document.querySelector("#alphabet")
+var numbersButton = document.querySelector("#numbers")
+var definitionsButton = document.querySelector("#definitions")
+var customButton = document.querySelector("#custom")
+
+alphabetButton.addEventListener("click", function(){
+	var alpha = document.getElementById("alphabet");
+	if (alpha.style.display )
+
+	console.log(alphabetButton);
+})
+
+numbersButton.addEventListener("click", function(){
+	var num = document.getElementById("alphabet");
+	if (num.style.display )
+
+	console.log(numbersButton);
+})
+
+definitionsButton.addEventListener("click", function(){
+	var def = document.getElementById("alphabet");
+	if (def.style.display )
+
+	console.log(definitionsButton);
+})
+
+customButton.addEventListener("click", function(){
+	var cust = document.getElementById("alphabet");
+	if (cust.style.display )
+
+	console.log(customButton);
+})
 
 
 // Letters Arrays
