d1e35abf4c1329cf2acf8037ae53e0bba9fe209f
Crothos <s.g.totten@gmail.com>
2023-01-13 12:10:05 -0600

    added a card to HTML & CSS to be able to put stuff in

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
+var container = document.getElementById("container");
 
+// Letters Array
+var alphaArray = ["A", "B", "C", "D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-// Letters Arrays
-var alphaUpperArray = ["A", "B", "C", "D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var alphaLowerArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 // Numbers Arrays
 const number = ["0","1","2","3","4","5","6","7","8","9","10",];
@@ -16,15 +16,19 @@ const random = Math.floor(Math.random() * number.length);
 
 
 // WordsAPI Fetch Request, Returns Random Word/Definition and Console Logs it
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '9c87cf3d42msh23dac74cec4328bp1b4dbdjsnc47d2a2101c4',
-		'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
-	}
-};
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '9c87cf3d42msh23dac74cec4328bp1b4dbdjsnc47d2a2101c4',
+// 		'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+// 	}
+// };
 
-fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+
+// Function to show Alphabet cards
+
