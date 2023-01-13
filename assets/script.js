3ed30a8c42cc58937ace35bc9db5c7a45c7cce56
jared-GG747 <jdgib18@gmail.com>
2023-01-13 13:51:44 -0600

    added numbers array and random function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
-function flashCard () {
-    // 
-
+let numbers = [];
+for (var i = 0; i <= 100; i++){
+    numbers.push(i);
 }
+
+function randNumber(arr) {
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+    
+}
+
+console.log(randNumber(numbers));
