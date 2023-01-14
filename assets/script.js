6d04282345d5f0e8e66005239fc5b8b1e3fc1d7f
Crothos <s.g.totten@gmail.com>
2023-01-13 20:34:14 -0600

    card displays word, console logs definition for now

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,15 @@
 var word;
+var definit;
 
 let numbers = [];
-for (var i = 0; i <= 100; i++){
-    numbers.push(i);
+for (var i = 0; i <= 100; i++) {
+	numbers.push(i);
 }
 
 function randNumber(arr) {
-    var randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
-    
+	var randomIndex = Math.floor(Math.random() * arr.length);
+	return arr[randomIndex];
+
 }
 console.log(randNumber(numbers));
 
@@ -20,33 +21,33 @@ var citiesButton = document.querySelector("#cities")
 
 alphabetButton.addEventListener("click", displayAlpha);
 
-numbersButton.addEventListener("click", function(){
+numbersButton.addEventListener("click", function () {
 	console.log(numbersButton);
 })
 
 definitionsButton.addEventListener("click", getWord);
 
-customButton.addEventListener("click", function(){
+customButton.addEventListener("click", function () {
 	console.log(customButton);
 })
 
-citiesButton.addEventListener("click", function(){
+citiesButton.addEventListener("click", function () {
 	console.log(citiesButton);
 })
 var container = document.getElementById("container");
 
 
 // Letters Array
-var alphaArray = ["A", "B", "C", "D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphaArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
 // Numbers Arrays
-const number = ["0","1","2","3","4","5","6","7","8","9","10",];
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",];
 
 
 // Getting random number from Array
 
-const number2 = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten",];
+const number2 = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",];
 
 const random = Math.floor(Math.random() * number.length);
 
@@ -54,31 +55,32 @@ const random = Math.floor(Math.random() * number.length);
 
 // WordsAPI Fetch Request, Returns Random Word/Definition and Console Logs it
 
-function getWord(){
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '9c87cf3d42msh23dac74cec4328bp1b4dbdjsnc47d2a2101c4',
-		'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
-	}
-};
+function getWord() {
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '9c87cf3d42msh23dac74cec4328bp1b4dbdjsnc47d2a2101c4',
+			'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+		}
+	};
 
-fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', options)
-	.then(function (response) {
-		return response.json();
-	})
-	.then(function(data) {
-		console.log(data)
-		word = data.word;
-	})
-	displayWord();
-
-	function displayWord(){
+	fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', options)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+			word = data.word;
+			definit = data.results[0].definition;
+			displayWord();
+		})
 	
-	// var definition = data.results[0].definition;
-	var cardBlank = document.getElementById("content1");
-	cardBlank.innerHTML = word;
-}
+
+	function displayWord() {
+		var cardBlank = document.getElementById("content1");
+		cardBlank.innerHTML = word;
+		console.log(definit);
+	}
 }
 
 // // Cities and Countries Fetch Request
@@ -95,16 +97,16 @@ fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', options)
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
 
-function displayAlpha(){
-    
+function displayAlpha() {
+
 
 	function getRandomItem(alphaArray) {
 		var randomIndex = Math.floor(Math.random() * alphaArray.length);
 		var item = alphaArray[randomIndex];
 		return item;
 	}
-	
+
 	var result = getRandomItem(alphaArray);
 	var cardBlank = document.getElementById("content1");
 	cardBlank.innerHTML = result;
-	}
+}
